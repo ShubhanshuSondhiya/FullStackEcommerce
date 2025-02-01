612081394b994c6a92a534cf8bbcb2db3bff661d
Shubhanshu Sondhiya <shubhanshusondhiya@gmail.com>
2025-02-01 20:10:34 +0530

    Folder sStructure

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api",ecomRouter);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server is up and Running at port: " + PORT)
+    );
+  })
+  .catch((err) => console.log(err));
